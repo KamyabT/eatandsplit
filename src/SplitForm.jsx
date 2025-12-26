78ae296ca64394e0b7d75056fefa15c83229d4d9
Kamya Tanhaie <dgghost034@gmail.com>
2025-12-26 18:39:30 +0330

    split form input states added and the logic

--- src/SplitForm.jsx
+++ src/SplitForm.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 const SplitForm = ({ selectedFriend }) => {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+  const paidByFriend = bill ? bill - paidByUser : "";
+
   return (
     <>
       <form
@@ -6,22 +13,45 @@ const SplitForm = ({ selectedFriend }) => {
         style={{ width: "480px" }}
         action=""
       >
-        <p>Split a bill with {selectedFriend.name}</p>
+        <p className="text-center h4">Split a bill with {selectedFriend.name}</p>
         <label className="d-flex align-items-center justify-content-between p-2">
-          <span>Bill amount:</span> <input className="form-control w-50" type="number" />
+          <span>Bill amount:</span>{" "}
+          <input
+            value={bill}
+            onChange={(e) => setBill(Number(e.target.value))}
+            className="form-control w-50"
+            type="number"
+          />
         </label>
         <label className="d-flex align-items-center justify-content-between p-2">
-          <span>Your Expense:</span> <input className="form-control w-50" type="number" />
+          <span>Your Expense:</span>{" "}
+          <input
+            value={paidByUser}
+            onChange={(e) => setPaidByUser(Number(e.target.value))}
+            className="form-control w-50"
+            type="number"
+          />
         </label>
         <label className="d-flex align-items-center justify-content-between p-2">
           <span>Friend Expense:</span>
-          <input className="form-control w-50" type="number" disabled />
+          <input
+            className="form-control w-50"
+            type="number"
+            value={paidByFriend}
+            disabled
+          />
         </label>
         <label className="d-flex align-items-center justify-content-between p-2">
           <span>Who is paying the bill?</span>
-          <select className="form-select p-2 w-50" name="" id="">
-            <option value="">You</option>
-            <option value={selectedFriend.name}>{selectedFriend.name}</option>
+          <select
+            value={whoIsPaying}
+            onChange={(e) => setWhoIsPaying(e.target.value)}
+            className="form-select p-2 w-50"
+            name=""
+            id=""
+          >
+            <option value="user">You</option>
+            <option value="friend">{selectedFriend.name}</option>
           </select>
         </label>
         <button className="btn btn-dark mt-3">Split Bill</button>
